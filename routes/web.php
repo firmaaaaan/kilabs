def6191c140165925e3877a0b5f02ff5c77681e7
firmaaaaan <fiermans2000@gmail.com>
2024-03-22 14:47:15 +0700

    Tampilan login dan register ditambahkan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function () {
+    return view('components.auth.login');
+});
+Route::get('/register', function () {
+    return view('components.auth.register');
 });
